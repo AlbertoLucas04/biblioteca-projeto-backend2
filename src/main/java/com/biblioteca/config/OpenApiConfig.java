173f011e28f8a730f3346139206a317f35509e53
Alberto Lucas Martins Alves <AlbertoLucas@academicos.com>
2026-06-03 20:04:43 -0300

    feat: corrigindo a dependencia do swagger

---
+++ src/main/java/com/biblioteca/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.biblioteca.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Biblioteca Digital API")
+                        .version("1.0.0")
+                        .description("API de Gerenciamento de Biblioteca Digital")
+                        .contact(new Contact()
+                                .name("Suporte")
+                                .email("suporte@biblioteca.com")));
+    }
+}
